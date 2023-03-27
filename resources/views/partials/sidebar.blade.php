39cfb9aa31c1346fb57c2d81efd5202c8aabd87a
Abdur Rafi <rafiridwan23@gmail.com>
2023-03-27 10:51:12 +0700

    logout button

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -53,16 +53,17 @@
           <span class="nav-link-text ms-1">RTL</span>
         </a>
       </li> --}}
-      <li class="nav-item mt-3">
-        <h6 class="ps-4 ms-2 text-uppercase text-xs font-weight-bolder opacity-6">Account pages</h6>
+      <li class="nav-item">
+        {{-- <h6 class="ps-4 ms-2 text-uppercase text-xs font-weight-bolder opacity-6">Account pages</h6> --}}
+        <hr class="horizontal dark">
       </li>
       <li class="nav-item">
-        <a class="nav-link {{ request()->segment(1) == 'profile' ? 'active' : '' }}" href="/profile">
+        <a class="nav-link" href="javascript:void(0)" onclick="signout()">
           <div
             class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
-            <i class="ni ni-single-02 text-dark text-sm opacity-10"></i>
+            <i class="ni ni-button-power text-danger text-sm opacity-10"></i>
           </div>
-          <span class="nav-link-text ms-1">Profile</span>
+          <span class="nav-link-text ms-1">Sign Out</span>
         </a>
       </li>
       {{-- <li class="nav-item">
