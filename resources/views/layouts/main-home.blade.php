e584fc2264e597d83bcb2aa8137a3709259609e5
Abdur Rafi <rafiridwan23@gmail.com>
2023-04-16 17:44:11 +0700

    hide clockwork

--- resources/views/layouts/main-home.blade.php
+++ resources/views/layouts/main-home.blade.php
@@ -41,7 +41,7 @@
 <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
 <script src="https://cdn.datatables.net/1.13.4/js/jquery.dataTables.min.js"></script>
 <script src="https://cdn.datatables.net/1.13.4/js/dataTables.bootstrap5.min.js"></script>
-<script src="https://cdn.jsdelivr.net/gh/underground-works/clockwork-browser@1/dist/toolbar.js"></script>
+{{-- <script src="https://cdn.jsdelivr.net/gh/underground-works/clockwork-browser@1/dist/toolbar.js"></script> --}}
 <script>
   // get the element to animate
   var element = document.getElementById('count-stats');
