e06067edec5c48318206fb2c76d1a66b0be59872
Abdur Rafi <rafiridwan23@gmail.com>
2023-04-16 17:24:56 +0700

    tuition fee table

--- resources/views/home/tuition.blade.php
+++ resources/views/home/tuition.blade.php
@@ -6,7 +6,7 @@
     <div class="col-md-12 p-3">
       <p class="fs-1 fw-bold text-warning">Tuition Fee</p>
       <p class="fs-3 text-dark">Tuition Fee<b class="fw-bold text-warning"> Odd Semester</b></p>
-      <table class="table table-bordered table-sm border-dark text-dark">
+      <table class="table table-light table-striped table-bordered">
           <tr>
               <th>Study Program</th>
               <th>Fixed Course Fee</th>
@@ -17,15 +17,15 @@
               <th>Total Fee</th>
           </tr>
           @foreach ($tuitionFeeOdd as $odd)
-          <tr style="border: 1px solid black;">
-              <td>{{ $odd->studyProgram->name }}</td>
-              <td>Rp{{number_format($odd->fixed_course_fee, 0, ".", ".") }}</td>
-              <td>Rp{{number_format($odd->variable_course_fee, 0, ".", ".") }}</td>
-              <td>Rp{{number_format($odd->practical_studio_fee, 0, ".", ".") }}</td>
-              <td>Rp{{number_format($odd->tuition_fee, 0, ".", ".") }}</td>
-              <td>Rp{{number_format($odd->educational_development_fee, 0, ".", ".") }}</td>
-              <td>Rp{{number_format($odd->total_fee, 0, ".", ".") }}</td>
-          </tr>
+            <tr>
+                <td>{{ $odd->studyProgram->name }}</td>
+                <td>Rp{{number_format($odd->fixed_course_fee, 0, ".", ".") }}</td>
+                <td>Rp{{number_format($odd->variable_course_fee, 0, ".", ".") }}</td>
+                <td>Rp{{number_format($odd->practical_studio_fee, 0, ".", ".") }}</td>
+                <td>Rp{{number_format($odd->tuition_fee, 0, ".", ".") }}</td>
+                <td>Rp{{number_format($odd->educational_development_fee, 0, ".", ".") }}</td>
+                <td>Rp{{number_format($odd->total_fee, 0, ".", ".") }}</td>
+            </tr>
           @endforeach
       </table>
     </div>
@@ -34,7 +34,7 @@
   <div class="row">
     <div class="col-md-12 table-responsive p-3">
       <p class="fs-3 text-dark">Tuition Fee<b class="fw-bold text-warning"> Even Semester</b></p>
-      <table class="table table-bordered table-sm border-dark text-dark">
+      <table class="table table-light table-striped table-bordered">
           <tr>
               <th>Study Program</th>
               <th>Fixed Course Fee</th>
