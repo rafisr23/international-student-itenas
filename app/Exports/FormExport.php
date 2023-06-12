8125d9152b3c17b80e621f6add3dc3fcb8eafce1
Abdur Rafi <rafiridwan23@gmail.com>
2023-06-12 09:48:19 +0700

    add status to excel

--- app/Exports/FormExport.php
+++ app/Exports/FormExport.php
@@ -24,7 +24,7 @@ class FormExport implements ShouldAutoSize, WithHeadings, FromQuery
 
     public function query()
     {
-        return Form::query()->join('students', 'forms.student_id', '=', 'students.id')->join('scholarships', 'forms.scholarship_id', '=', 'scholarships.id')->join('users', 'students.user_id', '=', 'users.id')->join('study_programs', 'forms.study_program_id', '=', 'study_programs.id')->join('faculties', 'study_programs.faculty_id', '=', 'faculties.id')->select('forms.reg_number', 'users.name', 'students.gender', 'students.phone_number', 'students.address', 'students.city', 'students.country', 'students.postal_code', 'forms.high_school', 'forms.grad_date', 'forms.school_address', 'forms.school_city', 'forms.school_country', 'forms.school_postal_code', 'study_programs.name as study_program', 'faculties.name as faculty', 'scholarships.name as scholarship')->where('forms.status', 'Form Review')->orderBy('users.name', 'asc');
+        return Form::query()->join('students', 'forms.student_id', '=', 'students.id')->join('scholarships', 'forms.scholarship_id', '=', 'scholarships.id')->join('users', 'students.user_id', '=', 'users.id')->join('study_programs', 'forms.study_program_id', '=', 'study_programs.id')->join('faculties', 'study_programs.faculty_id', '=', 'faculties.id')->select('forms.reg_number', 'users.name', 'students.gender', 'students.phone_number', 'students.address', 'students.city', 'students.country', 'students.postal_code', 'forms.high_school', 'forms.grad_date', 'forms.school_address', 'forms.school_city', 'forms.school_country', 'forms.school_postal_code', 'study_programs.name as study_program', 'faculties.name as faculty', 'scholarships.name as scholarship', 'forms.status')->orderBy('users.name', 'asc');
     }
 
     public function headings(): array
@@ -46,7 +46,8 @@ class FormExport implements ShouldAutoSize, WithHeadings, FromQuery
             'Kode Pos Sekolah',
             'Program Studi',
             'Fakultas',
-            'Beasiswa'
+            'Beasiswa',
+            'Status'
         ];
     }
 }
