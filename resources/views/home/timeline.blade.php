e7c29a985f4a564646f31833ee4247d912ce91b6
Abdur Rafi <rafiridwan23@gmail.com>
2024-01-11 10:06:02 +0700

    update regis timeline

--- resources/views/home/timeline.blade.php
+++ resources/views/home/timeline.blade.php
@@ -7,7 +7,7 @@
         <p class="fs-1 fw-bold text-warning">Registration Timeline</p>
         <div class="row mb-5">
           <div class="col-md-12">
-            <img src="/assets/img/important-dates.png" alt="" class="border-radius-lg" height="400">
+            <img src="/assets/img/important-dates-v2.png" alt="" class="border-radius-lg" height="400">
           </div>
         </div>
         <table class="table table-light table-striped table-bordered">
@@ -20,31 +20,31 @@
           <tbody>
             <tr>
               <th>Online Registration</th>
-              <td>17 April - 31 May 2023</td>
+              <td>5 February - 30 April 2024 </td>
             </tr>
             <tr>
               <th>Online Interview</th>
-              <td>9-16 June 2023</td>
+              <td>13 - 17 May 2024</td>
             </tr>
             <tr>
               <th>Selection Result Announcement</th>
-              <td>23 June 2023</td>
+              <td>31 May 2024</td>
             </tr>
             <tr>
               <th>New Student Registration</th>
-              <td>26 June - 8 July 2023</td>
+              <td>3 - 14 June 2024</td>
             </tr>
             <tr>
               <th>Tuition Fee and Institution Development Donation Payment</th>
-              <td>10 - 15 July 2023</td>
+              <td>17 - 21 June 2024</td>
             </tr>
             <tr>
               <th>New Student Convocation</th>
-              <td>9 September 2023</td>
+              <td>6 September 2024</td>
             </tr>
             <tr>
               <th>First day of lecture</th>
-              <td>18 September 2023</td>
+              <td>17 September 2024</td>
             </tr>
           </tbody>
         </table>
